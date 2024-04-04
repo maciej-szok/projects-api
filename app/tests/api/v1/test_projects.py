8f45859cd259aba3ca097eafb091a88a192838c8
Maciej <maciej.szok12@gmail.com>
2024-04-04 18:41:56 +0200

    feat: add tests for the rest of the API methods

--- app/tests/api/v1/test_projects.py
+++ app/tests/api/v1/test_projects.py
@@ -45,3 +45,52 @@ def test_read_project(
     assert content["date_range"]['lower'] == str(project.date_range.lower)
     assert content["date_range"]['upper'] == str(project.date_range.upper)
     assert content["id"] == project.id
+
+
+def test_read_projects(
+    client: TestClient, db: Session
+) -> None:
+    project1 = create_random_project(db)
+    project2 = create_random_project(db)
+    response = client.get(f"{settings.API_V1_PATH}/projects")
+    assert response.status_code == 200
+
+    content = response.json()
+    assert 'items' in content
+
+
+def test_update_project(client: TestClient, db: Session):
+    project = create_random_project(db)
+    data = {
+        'name': 'Example test 🚀',
+        'description': "Some description",
+        'date_range': {
+            'lower': '2021-01-01',
+            'upper': '2021-01-02'
+        },
+        "area_of_interest": {"field1": "ok"}
+    }
+
+    response = client.put(
+        f"{settings.API_V1_PATH}/projects/{project.id}", json=data,
+    )
+
+    assert response.status_code == 200
+    content = response.json()
+    assert content["name"] == data["name"]
+    assert content["description"] == data["description"]
+    assert content["date_range"]['lower'] == data["date_range"]['lower']
+    assert content["date_range"]['upper'] == data["date_range"]['upper']
+    assert content["area_of_interest"] == data["area_of_interest"]
+    assert 'id' in content
+
+
+def test_delete_project(client: TestClient, db: Session):
+    project = create_random_project(db)
+    response = client.delete(f"{settings.API_V1_PATH}/projects/{project.id}")
+    assert response.status_code == 200
+    content = response.json()
+    assert content["id"] == project.id
+
+    response_get = client.get(f"{settings.API_V1_PATH}/projects/{project.id}")
+    assert response_get.status_code == 404
--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
 export $(grep -v '^#' .env | xargs)
 
 # TODO create and remove a test database locally
